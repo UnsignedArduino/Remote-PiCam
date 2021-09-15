f63655dd8adea445d7b53d6db69fd4c698e9fb58
UnsignedArduino <UnsignedArduino@outlook.com>
2021-09-15 21:51:31 +0100

    Can change camera name and port from JSON file

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import logging
+from json import loads as load_json, dumps as dump_json
+from pathlib import Path
 
 import picamera
-
 from create_logger import create_logger
 from picam import NetworkPiCam
 
 logger = create_logger(name=__name__, level=logging.DEBUG)
 
-cam_name = "picam"
-port = 7896
+SETTINGS_PATH = Path.cwd() / "settings.json"
+
+defaults = {
+    "camera": {
+        "name": "picam",
+        "port": 7896
+    },
+    "pan_tilt": {
+        "enable": True
+    }
+}
+
+logger.debug(f"Loading settings from {SETTINGS_PATH}")
+if not SETTINGS_PATH.exists():
+    logger.warning("Settings file does not exist, creating!")
+    SETTINGS_PATH.write_text(dump_json(defaults, indent=4))
+settings = {**defaults, **load_json(SETTINGS_PATH.read_text())}
 
 try:
     with picamera.PiCamera() as camera:
         camera.resolution = (720, 480)
-        cam = NetworkPiCam(camera, cam_name, port)
+        cam = NetworkPiCam(camera, settings["camera"]["name"],
+                           settings["camera"]["port"])
         logger.debug("Attempting to connect")
         while not cam.connect(timeout=3):
             pass
